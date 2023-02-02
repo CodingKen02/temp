aeb1c22d2e306d1071ad582b97db3ea5473e3abb
CodingKen02 <71536021+CodingKen02@users.noreply.github.com>
2023-02-02 10:57:30 -0600

    Initial commit

---
+++ lec4.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title> Lecture 4 - PHP Hello Wolrd </title>
+  <link rel="stylesheet" href=""> 
+  <!-- ----------------------------------
+			BIS1523/BIS2523 Documentation
+   Name: Kennedy Keyes
+   Netid: kfk38
+   Date: 2/2/2023
+   
+   Variables used:
+      <variable name>  <description of data variable will hold>
+      <$temp>          <holds the data for temperature>
+      <$result>        <is the result of division>
+  
+   -------------------------------------  -->
+</head>
+<body>
+    
+    <?
+    
+    print "<h1>Hello World!</h1>";
+    print "This is my first PHP program! <br>";
+    
+    $temp = 76;
+    $result = $temp / 2;
+    print "Your room's current temperature is: $temp F <br>";
+    print "The divided temperature is: $result F";
+    
+    ?>
+
+</body>
+</html>
